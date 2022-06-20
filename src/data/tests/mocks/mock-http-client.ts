f0151a31ca0e0846500cf0b0f5863d2e1b40a95e
SesisnandoLRNeto <nandolrneto@gmail.com>
2022-06-19 21:27:43 -0400

    test: ensure RemoteAuthentication throw InvalidCredentialsError on 401

--- src/data/tests/mocks/mock-http-client.ts
+++ src/data/tests/mocks/mock-http-client.ts
@@ -2,15 +2,22 @@ import {
   HttpPostClient,
   HttpPostParams,
 } from "@/data/protocols/http/http-post-client";
+import {
+  HttpResponse,
+  HttpStatusCodeType,
+} from "@/data/protocols/http/http-responses";
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
   body?: unknown;
+  result?: HttpResponse = {
+    statusCode: HttpStatusCodeType.noContent,
+  };
 
-  async post(params: HttpPostParams): Promise<void> {
+  async post(params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url;
     this.body = params.body;
 
-    return Promise.resolve();
+    return Promise.resolve(this.result);
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { HttpPostClientSpy } from "@/data/tests/mocks/mock-http-client";
 import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication";
 import { faker } from "@faker-js/faker";
 import { mockAuthentication } from "@/domain/tests/mocks/mock-authentication";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
+import { HttpStatusCodeType } from "@/data/protocols/http/http-responses";
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -33,5 +35,18 @@ describe("RemoteAuthentication", () => {
     await sut.auth(authenticationParams);
 
     expect(httpPostClientSpy.body).toEqual(authenticationParams);
+  });
+
+  test("Should throw InvalidCredentialsError if HttpPostClient returns 401", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.result = {
+      statusCode: HttpStatusCodeType.unauthorized,
+    };
+
+    const authenticationParams = mockAuthentication();
+    const promise = sut.auth(authenticationParams);
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
